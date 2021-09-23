16737d02c015d325c97fb2c8a7ef818437ab2dc5
Ronaldo Queiroz <ronquesil@gmail.com>
2021-09-22 22:51:32 -0300

    Primeiros Exercícios do Curso
    Primeiros Exercícios do Curso

---
+++ Testes/Exemplo01/TipoVariavel.py
@@ -0,0 +1,13 @@
+codigo = 10
+salario = 1500.00
+nome = 'José'
+#situação = True
+
+#tipo = type (salario)
+
+#print(salario)
+#print(tipo)
+
+#print('Código: ', codigo, 'Nome: ', nome, 'O salário atual é de ', salario)
+
+print('Código: ' + str (codigo)+ 'Nome: '+ nome+ ' O salário atual é de '+ str(salario))
